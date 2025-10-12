4f309bff915be4e716c19bf04610e2ccbfc1cf91
Esma <esmanur1234kilic@gmail.com>
2025-10-12 15:01:34 +0200

    added includes for navbar and footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -65,40 +65,6 @@
         pointer-events:none;
     }
 
-    nav {
-        contain: paint;
-
-        top: 0;
-        left: 0;
-        background-color: var(--primary);
-        padding: 20px 0;
-        text-align: center;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        z-index: 10;
-
-    }
-
-
-    nav a {
-        text-decoration: none;
-        color: var(--white);
-        font-size: 15px;
-        margin: 0 10px;
-        transition: all 0.3s ease;
-        position: relative;
-    }
-
-    nav a:hover {
-        border-bottom: 2px solid white;
-        padding-bottom: 5px;
-    }
-
-    .logo {
-        max-width: 150px;
-        margin: -15px 20px -15px 0;
-    }
 
     .viewport-header {
         position: absolute;
@@ -338,15 +304,7 @@
       <div class="video-overlay"></div>
     </div>
 
-    <nav>
-      <img src="logo.png" alt="Futuro Logo" class="logo">
-      <a href="#" class="active">HOME</a>
-      <a href="#">RONDVAARTEN</a>
-      <a href="#">ARRANGEMENTEN</a>
-      <a href="#">OVER FUTURO</a>
-      <a href="#">RESERVEREN</a>
-      <a href="#">CONTACT</a>
-    </nav>
+@include('includes.navbar')
 
     <div class="viewport-header">
       <div class="hero-text">BOEK VANDAAG NOG UW RONDVAART</div>
@@ -462,6 +420,8 @@ alleen varen.
 
 <a href="{{ route('booking') }}" class="floating-button">Reserveer nu</a>
 
+
+@include('includes.footer')
 
 <script>
   window.addEventListener('load', () => {
