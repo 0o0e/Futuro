f6c6cbb9175677c5eb64e88ad9e9ac1c6c61a612
LarsKalishoek <99069312@mydavinci.nl>
2025-11-17 21:00:01 +0100

    Added discount code making through admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\BookingController;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\OwnerMiddleware;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Admin\DiscountCodeController;
 
 
 Route::get('/', function () {
@@ -35,7 +36,8 @@ Route::middleware(AdminMiddleware::class)->prefix('admin')->group(function () {
     Route::get('/boeking/aanmaken', [Admincontroller::class, 'createReservation'])->name('admin.reservation.create');
     Route::post('/boeking/aanmaken', [Admincontroller::class, 'storeReservation'])->name('admin.reservation.store');
 
-
+    Route::post('/discount-codes', [DiscountCodeController::class, 'store'])->name('admin.discount-codes.store');
+    Route::get('/discount-codes', [DiscountCodeController::class, 'index'])->name('admin.discount-codes.index');
 });
 
 Route::middleware(OwnerMiddleware::class)->prefix('admin')->group(function () {
@@ -45,4 +47,3 @@ Route::middleware(OwnerMiddleware::class)->prefix('admin')->group(function () {
 
 
 });
-
