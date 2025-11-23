ff2192d287b170699c400fe2c4bdc077e93cb545
Esma <esmanur1234kilic@gmail.com>
2025-11-23 23:10:59 +0100

    made a pdf view with css to make it look like a giftcard and added the pdf as attachement to the vaardebon mailable

--- app/Mail/VaardebonMail.php
+++ app/Mail/VaardebonMail.php
@@ -10,6 +10,9 @@ use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Address;
 use App\Models\DiscountCode;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Mail\Mailables\Attachment;
+
 class VaardebonMail extends Mailable
 {
     use Queueable, SerializesModels;
@@ -40,6 +43,10 @@ class VaardebonMail extends Mailable
     {
         return new Content(
             view: 'emails.vaardebon',
+        with: [
+            'discount_code' => $this->discount_code,
+        ]
+
         );
     }
 
@@ -50,6 +57,14 @@ class VaardebonMail extends Mailable
      */
     public function attachments(): array
     {
-        return [];
+
+        $pdf = Pdf::loadView("emails.vaardebon-pdf",[
+            "discount_code" => $this->discount_code,
+
+        ])->output();
+        return [
+            Attachment::fromData(fn () => $pdf, 'vaardebon.pdf')
+                ->withMime('application/pdf'),
+        ];
     }
 }
